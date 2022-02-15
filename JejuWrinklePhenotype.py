b0d2e6c2c41eff6dfcfed1c9fc8e9c1cd8426e9c
ujjjjjang <s2kstd14@naver.com>
2022-02-15 17:52:39 +0900

    commit

--- JejuWrinklePhenotype.py
+++ JejuWrinklePhenotype.py
@@ -1,21 +1,42 @@
 from xml.dom import WrongDocumentErr
 import pandas as pd
 import numpy as np
-import openpyxl
-
+import openpyxl as opxl
 wrinkle_data = pd.read_excel('G:/내 드라이브/Yuchang-Practice/Data_coding_ver.xlsx')
+#put 'W_Total' in column. W_TOTAL is added result of W101 to W104
+W1 = []
+for i in wrinkle_data['W101']:
+    if 0 <= i and i < 17.89 : W1.append(1)
+    elif 17.89 <= i and i < 22.34 : W1.append(2)
+    else : W1.append(3)
+W2 = []
+for i in wrinkle_data['W102']:
+    if 0 <= i and i < 157.83 : W2.append(1)
+    elif 157.83 <= i and i < 213.25 : W2.append(2)
+    else : W2.append(3)
+W3 = []
+for i in wrinkle_data['W103']:
+    if 0 <= i and i < 21.19 : W3.append(1)
+    elif 21.19 <= i and i < 27.75 : W3.append(2)
+    else : W3.append(3)
+W4 = []
+for i in wrinkle_data['W104']:
+    if 0 <= i and i < 155.36 : W4.append(1)
+    elif 155.36 <= i and i < 206.54 : W4.append(2)
+    else : W4.append(3)
 
-wrinkle1 = wrinkle_data.filter(['W101', 'W102', 'W103', 'W104'])
-#put 'W_Total' in column. W_Total is added result of W101 to W104
+W_Total = []
+for i in range(0, len(W1)):
+     W_Total.append(W1[i] + W2[i] + W3[i] + W4[i])
 
-type("W101")
-print("W101")
-print(wrinkle_data['W101'])
-
+print(W_Total)
 #output
+#print(wrinkle_data.filter(['W101', 'W102', 'W103', 'W104','W_TOTAL']))
+
+
 
 # W101
-# 1: 0 <= x < 17.89
+# 1: 1 if 0 <= x < 17.89
 # 2: 17.89 <= x < 22.34
 # 3: 22.34 <= x
 # W102
